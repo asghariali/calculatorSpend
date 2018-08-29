b2ee87e6874f2611f736e34319ec631bbfb19bb7
ali asghari <alipilot2006@live.de>
2018-08-29 22:09:45 +0200

    Add calculation component
    #4

---
+++ src/components/calculation.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Calculation extends Component {
+  render() {
+    return <h1>Hello world !</h1>;
+  }
+}
+
+export default Calculation;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import Navbar from "./components/navbar";
+import Calculation from "./components/calculation";
 
 ReactDOM.render(<Navbar />, document.getElementById("navbar"));
+ReactDOM.render(<Calculation />, document.getElementById("calculation"));
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
