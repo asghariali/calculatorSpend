1977bd649270290613e557596e792bd324a6b15c
ali asghari <alipilot2006@live.de>
2018-08-29 22:12:31 +0200

    delete app component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import Navbar from "./components/navbar";
@@ -9,5 +8,4 @@ import Calculation from "./components/calculation";
 
 ReactDOM.render(<Navbar />, document.getElementById("navbar"));
 ReactDOM.render(<Calculation />, document.getElementById("calculation"));
-ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
